801b919eb9a22d274ad3a06dbf3db1f96165d6ff
olivier-boesch <olivier.boesch@ac-aix-marseille.fr>
2020-11-02 08:45:51 +0100

    first version (0.9)

---
+++ src/gen_pdf.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+# coding: utf8
+from collections import OrderedDict
+from datetime import datetime, timedelta
+from fpdf import FPDF
+import os.path
+
+title = "ATTESTATION DE DÉPLACEMENT DÉROGATOIRE"
+
+prelude = "En application du décret n°2020-1310 du 29 octobre 2020 prescrivant les mesures générales nécessaires pour " \
+          "faire face à l'épidémie de Covid19 dans le cadre de l'état d'urgence sanitaire "
+
+debut = u"Je soussigné(e),\nMme/M {nom:s} {prenom:s}\nné(e) le : {date_naissance:s} à {lieu_naissance:s}\nDeumeurant : {" \
+        "adresse:s} {code_postal:s} {commune:s}\ncertifie que mon déplacement est lié au motif suivant autorisé par " \
+        "le décret n°2020-1310 du 29 octobre 2020 prescrivant les mesures générales nécessaires pour faire face à " \
+        "l'épidémie de Covid19 dans le cadre de l'état d'urgence sanitaire  : "
+
+motifs = OrderedDict([
+        ("Déplacement Domicile-Travail", "Déplacements entre le domicile et le lieu d'exercice de l'activité "
+                                         "professionnelle ou un établissement d'enseignement ou de formation, "
+                                         "déplacements professionnels ne pouvant être différés , déplacements pour "
+                                         "un concours ou un examen. Déplacements pour effectuer des achats de "
+                                         "fournitures nécessaires à l'activité professionnelle, des achats de "
+                                         "première nécessité  dans des établissements dont les activités demeurent "
+                                         "autorisées, le retrait de commande et les livraisons à domicile;"),
+        ("Déplacement pour achats", "Déplacements pour effectuer des achats de fournitures nécessaires à l'activité "
+                                    "professionnelle, des achats de première nécessité  dans des établissements "
+                                    "dont les activités demeurent autorisées, le retrait de commande et les "
+                                    "livraisons à domicile;"),
+        ("Consultation médicale", "Consultations, examens et soins ne pouvant être assurés à distance et l'achat de "
+                                  "médicaments;"),
+        ("Motif familial impérieux", "Déplacements pour motif familial impérieux, pour l'assistance aux personnes "
+                                     "vulnérables et précaires ou la garde d'enfants;"),
+        ("Personne en situtation de handicap", "Déplacement des personnes en situation de handicap et leur "
+                                               "accompagnant;"),
+        ("Activité physique", "Déplacements brefs, dans la limite d'une heure quotidienne et dans un rayon maximal "
+                              "d'un kilomètre autour du domicile, liés soit à l'activité physique individuelle des "
+                              "personnes, à l'exclusion de toute pratique sportive collective et de toute proximité "
+                              "avec d'autres personnes, soit à la promenade avec les seules personnes regroupées dans "
+                              "un même domicile, soit aux besoins des animaux de compagnie;"),
+        ("Convocation administrative", "Convocation judiciaire ou administrative et pour se rendre dans un service "
+                                       "public;"),
+        ("Mission d'intérêt général", "Participation à des missions d'intérêt général sur demande de l'autorité "
+                                      "administrative;"),
+        ("École et périscolaire", "Déplacement pour chercher les enfants à l'école et à l'occasion de leurs activités "
+                                  "périscolaires;")])
+
+fin = "Fait à {commune:s}\nle : {date:s} à {heure:s}"
+
+motifs_courts = list(motifs.keys())
+motifs_longs = [motifs[k] for k in motifs.keys()]
+
+
+class PDF(FPDF):
+    pass
+
+
+def generer_pdf(save_dir, data, motif, urgence=False):
+    instant = datetime.now()
+    if urgence:
+        try:
+            delta = int(data['decalage_help'])
+        except ValueError:
+            delta = 0
+        instant = instant - timedelta(minutes=delta)
+    date = instant.strftime("%d %B %Y")
+    data['date'] = date
+    heure = instant.strftime("%H:%M")
+    data['heure'] = heure
+    header = debut.format(**data)
+    footer = fin.format(**data)
+    pdf = PDF(orientation='P', unit="mm", format='A4')
+    pdf.set_margins(left=15, top=20, right=13)
+    pdf.add_page()
+    pdf.set_font(family='Times', style='B', size=20)
+    pdf.multi_cell(w=0,h=20,txt=title)
+    pdf.set_font('Arial', '', 10)
+    pdf.multi_cell(0, 5, header)
+    pdf.ln()
+    for i in range(len(motifs_longs)):
+        if i == motif:
+            pdf.cell(w=5,h=5,border=1, txt="X")
+        else:
+            pdf.cell(w=5,h=5,border=1, txt=" ")
+        pdf.set_x(30)
+        pdf.multi_cell(w=0,h=5,txt=motifs_longs[i])
+        pdf.ln()
+    pdf.multi_cell(0, 5, footer)
+    pdf.ln()
+    pdf.output(name=os.path.join(save_dir, instant.strftime("%Y-%m-%d_%H-%M-%S.pdf")), dest='F')
+    return os.path.join(save_dir, instant.strftime("%Y-%m-%d_%H-%M-%S.pdf"))
+
+if __name__ == "__main__":
+    print(motifs_courts)
+    print(motifs_longs)
+    data = {'nom': 'Tims', 'prenom': 'Carl', 'adresse': "pas d'adresse", 'commune': 'Nowhere', 'code_postal': '12345', 'date_naissance': '01/02/2003', 'lieu_naissance': 'truc', 'decalage_help': '20'}
+    generer_pdf('.', data, motif=1, urgence=True)
+
+
---
+++ src/main.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+# coding: utf8
+from kivy.app import App
+from kivy.factory import Factory
+from kivy.logger import Logger
+from kivy.storage.jsonstore import JsonStore
+from gen_pdf import motifs_courts, generer_pdf
+import os.path
+from kivy import platform
+if platform == 'android':
+    from android.permissions import request_permissions, Permission
+    from android.storage import primary_external_storage_path, app_storage_path
+    import android
+    data_dir = app_storage_path()
+    user_dir = primary_external_storage_path()
+else:
+    from pathlib import Path
+    data_dir = os.path.dirname(os.path.abspath(__file__))
+    user_dir = Path.home()
+
+
+__version__ = "0.9"
+
+
+class AttgenApp(App):
+    data = {'nom': '',
+            'prenom': '',
+            'date_naissance': '',
+            'lieu_naissance': '',
+            'adresse': '',
+            'code_postal': '',
+            'commune': '',
+            'motif_defaut': '1',
+            'motif_help': '5',
+            'decalage_help': '20'}
+
+    def show_settings_popup(self):
+        p = Factory.SettingsPopup()
+        p.open()
+        for k in self.data.keys():
+            if 'motif' in k:
+                p.ids[k].text = motifs_courts[int(self.data[k])]
+            else:
+                try:
+                    p.ids[k].text = self.data[k]
+                except KeyError:
+                    pass
+
+    def on_dismiss_settings_popup(self, pop):
+        for k in self.data.keys():
+            try:
+                if 'motif' in k:
+                    self.data[k] = motifs_courts.index(pop.ids[k].text)
+                else:
+                    self.data[k] = pop.ids[k].text
+            except KeyError:
+                pass
+
+    def show_about(self):
+        p = Factory.AboutPopup()
+        p.open()
+
+    def load_data(self):
+        Logger.info("Data: Loading data...")
+        store = JsonStore(os.path.join(data_dir, 'data.json'))
+        for k in store.keys():
+            self.data[k] = store.get(k)['val']
+        Logger.info("Data: Loading data done {:s}".format(str(self.data)))
+
+    def save_data(self):
+        Logger.info("Data: Saving data...")
+        store = JsonStore(os.path.join(data_dir, 'data.json'))
+        for k in self.data.keys():
+            store.put(k, val=self.data[k])
+        Logger.info("Data: Saving data done")
+
+    def generer_urgence(self):
+        self.generer(urgence=True)
+
+    def generer(self, urgence=False):
+        if platform == 'android':
+            ret = request_permissions([Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE])
+        try:
+            if not urgence:
+                motif = self.root.cur_motif
+            else:
+                motif = self.data['motif_help']
+            name = generer_pdf(save_dir=user_dir, data=self.data, motif=motif, urgence=urgence)
+            if platform == 'android':
+                pass
+            else:
+                import webbrowser
+                webbrowser.open_new(os.path.join(user_dir, name))
+        except PermissionError:
+            pass
+
+    def on_start(self):
+        self.load_data()
+        self.root.ids['motif'+str(self.data['motif_defaut'])].state = 'down'
+
+    def on_stop(self):
+        self.save_data()
+
+
+if __name__ == "__main__":
+    app = AttgenApp()
+    app.run()
